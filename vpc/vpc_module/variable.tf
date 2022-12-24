e0805fd6edeadd5354e2a15f71ffe2a076bcbc94
BrouKoli <broukolifrancis@gmail.com>
2022-12-24 01:12:01 -0500

    brou push vpc

---
+++ vpc/vpc_module/variable.tf
@@ -0,0 +1,18 @@
+variable "cidr_block" {
+  type = string
+  # default = "10.10.0.0/16" 
+
+}
+
+variable "aws-region" {
+  type = string
+  # default = "us-east-1"
+
+}
+# variable "cidr_block" {
+#   type = string
+#   # default = "10.10.1.0/24"
+
+# }
+
+
